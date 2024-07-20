ad36867433f37d2e7848b5860d4f1ac7dca3ed87
alphaom33 <alphaom34@gmail.com>
2024-07-20 13:27:22 -0500

    maybe

--- app/ui/table.tsx
+++ app/ui/table.tsx
@@ -5,7 +5,7 @@ export default function Table() {
 			<table className="text-white w-2/3 relative left-[16%]">
 				<tbody className="bg-gray-900">
 				{things.map((thing: string) => (
-					<tr className="border-b last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg">
+					<tr key={thing} className="border-b last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg">
 						<td className="whitespace-nowrap py-3 pl-6 pr-3">
 							<div className="flex items-center gap-3">
 								<p>{thing}</p>
